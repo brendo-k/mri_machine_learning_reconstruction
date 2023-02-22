5cb6553092f6491a265d430e38097d6e6542121b
brenden <brenden.kadota@gmail.com>
2023-02-22 16:31:36 -0500

    Added fft and ifft methods for both np and Tensors

--- Utils/fft_2d_img.py
+++ Utils/fft_2d_img.py
@@ -1,9 +1,32 @@
-import numpy.fft as fft
+import numpy as np
+import torch
 import pyfftw
 
 
+def ifft_2d_img(data, axes=[2, 3]):
+    if isinstance(data, torch.Tensor):
+        ifft2 = torch.fft.ifft2
+        ifftshift = torch.fft.ifftshift
+        fftshift = torch.fft.fftshift
+    if isinstance(data, np.ndarray):
+        ifft2 = np.fft.ifft2
+        ifftshift = np.fft.ifftshift
+        fftshift = np.fft.fftshift
+    data_shifted = ifftshift(data, axes=axes)
+    image = ifft2(data_shifted, axes=axes)
+    image = fftshift(image, axes=axes)
+    return image
+
 def fft_2d_img(data, axes=[2, 3]):
-    data_shifted = fft.ifftshift(data, axes=axes)
-    image = pyfftw.interfaces.numpy_fft.fft2(data_shifted, axes=axes)
-    image = fft.fftshift(image, axes=axes)
+    if isinstance(data, torch.Tensor):
+        fft2 = torch.fft.fft2
+        ifftshift = torch.fft.ifftshift
+        fftshift = torch.fft.fftshift
+    if isinstance(data, np.ndarray):
+        fft2 = np.fft.fft2
+        ifftshift = np.fft.ifftshift
+        fftshift = np.fft.fftshift
+    data_shifted = fftshift(data, axes=axes)
+    image = fft2(data_shifted, axes=axes)
+    image = ifftshift(image, axes=axes)
     return image
