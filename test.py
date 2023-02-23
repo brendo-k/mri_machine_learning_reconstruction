05eca90aa7e1bc567e047dce60c45ce6b0ee368c
Brenden <brenden.kadota@gmail.com>
2023-02-23 13:30:07 -0500

    Removed training files

--- test.py
+++
@@ -1,8 +0,0 @@
-from Models import varnet
-import torch
-
-if __name__ == "__main__":
-    x = torch.rand((10, 1, 640, 320), dtype=torch.complex64)
-    mask = torch.ones((10, 1, 640, 320))
-    net = varnet.VarNet(2, 2)
-    net(x, mask)
