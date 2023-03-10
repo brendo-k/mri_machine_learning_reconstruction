e3681bc729fb7771632652c6499dac1b695e7ea1
brenden <brenden.kadota@gmail.com>
2023-03-10 16:59:48 -0500

    Added acceleration factor and acs_width

--- Dataset/undersampled_dataset.py
+++ Dataset/undersampled_dataset.py
@@ -4,7 +4,7 @@ import numpy as np
 from .FileReader.read_h5 import H5FileReader
 
 class UndersampledKSpaceDataset(KSpaceDataset):
-    def __init__(self, h5_directory, acs_width=30, transforms=None):
+    def __init__(self, h5_directory, acs_width=30, R=8, transforms=None):
         # call super constructor 
         super().__init__(h5_directory)
         # define our file reader
@@ -12,6 +12,7 @@ class UndersampledKSpaceDataset(KSpaceDataset):
         self.acs_width = acs_width
         # add transforms
         self.transforms = transforms
+        self.R = R
 
     def __getitem__(self, index):
         data = super().__getitem__(index)
@@ -40,12 +41,17 @@ class UndersampledKSpaceDataset(KSpaceDataset):
     def apply_undersampled_indecies(self, k_space, random_indecies):
         undersampled = np.copy(k_space)
         undersampled[:, :, :, random_indecies] = 0
+
         return undersampled
 
     def get_undersampled_indecies(self, k_space_size):
         center = int(k_space_size[1]//2)
-        acs_bounds = [(center - self.acs_width),(center + self.acs_width)]
+        acs_bounds = [(center - self.acs_width//2),(center + np.ceil(self.acs_width//2).astype(int))]
         sampled_indeces = [index for index in range(k_space_size[1]) if index not in range(acs_bounds[0],acs_bounds[1])]
-        random_indecies = random.choices(sampled_indeces, k=k_space_size[1]//2)
-        return random_indecies
+        random_indecies = random.choices(sampled_indeces, k=k_space_size[1]//self.R)
+        random_indecies = np.concatenate((random_indecies, range(acs_bounds[0], acs_bounds[1])))
+
+        all_indexes = np.arange(0, k_space_size[1])
+        undersampled_indeces = np.setdiff1d(all_indexes, random_indecies) 
+        return undersampled_indeces
         
