9316814701cac17fd2d3780e2e6e148e8518baeb
Brenden <brenden.kadota@gmail.com>
2023-04-11 13:49:10 -0400

    Extracted collate function into own method

--- ml_recon/Utils/collate_function.py
+++ ml_recon/Utils/collate_function.py
@@ -5,11 +5,12 @@ def collate_fn(data: dict[str, torch.tensor]):
     k_space = [d['k_space'] for d in data]
     ismrmrd_header = [d['ismrmrd_header'] for d in data]
     mask = [d['mask'] for d in data]
-    recon_rss = [d['reconstruction_rss'] for d in data]
+    recon_rss = [d['recon'] for d in data]
 
     undersampled = torch.concat(undersampled, dim=0)
     k_space = torch.concat(k_space, dim=0)
     mask = torch.concat(mask, dim=0)
+    recon_rss = torch.concat(recon_rss, dim=0)
 
     data = {
         'undersampled': undersampled, 
