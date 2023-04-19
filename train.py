8f54e8495d55a11574c05ddc79fe98da0e139d72
Brenden <brenden.kadota@gmail.com>
2023-04-19 15:07:45 -0400

    modified trainig files

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
-# %%
-from ml_recon.Models.Unet import Unet
+
+from ml_recon.Models.fastMRI_unet import Unet
 from torch.utils.data import DataLoader
-from ml_recon.Transforms import (pad, combine_coil, toTensor, addChannels, 
+from ml_recon.Transforms import (pad, trim_coils, combine_coil, toTensor, permute, 
                                    view_as_real, fft_2d, normalize, pad_recon)
 from ml_recon.Dataset.undersampled_dataset import UndersampledKSpaceDataset
 from torchvision.transforms import Compose
@@ -11,58 +11,45 @@ from ml_recon.Utils.collate_function import collate_fn
 from datetime import datetime
 from ml_recon.Utils.save_model import save_model
 
-# %%
+
 torch.manual_seed(0)
 np.random.seed(0)
 
-# %%
+
 from torch.utils.tensorboard import SummaryWriter
 writer = SummaryWriter('/home/kadotab/scratch/runs/' +  datetime.now().strftime("%Y%m%d-%H%M%S"))
 
-# %%
 
-# %%
+
+
 transforms = Compose(
     (
         pad((640, 320)),
         pad_recon((320, 320)), 
-        fft_2d(axes=[-1, -2]),
+        fft_2d(axes=[2,3]),
         combine_coil(),
         toTensor(),
         normalize(), 
-        addChannels(),
         view_as_real(),
+        permute() 
     )
 )
-dataset = UndersampledKSpaceDataset('/home/kadotab/projects/def-mchiew/kadotab/Datasets/fastMRI/multicoil_train', transforms=transforms, R=2)
+dataset = UndersampledKSpaceDataset('/home/kadotab/projects/def-mchiew/kadotab/Datasets/fastMRI/multicoil_train', transforms=transforms, R=4)
 dataloader = DataLoader(dataset, batch_size=1, collate_fn=collate_fn)
     
 
-# %%
-data = next(iter(dataloader))
 
-# %%
-data['undersampled']
-
-# %%
-import matplotlib.pyplot as plt
-plt.imshow(data['undersampled'][0].pow(2).sum(0).sqrt())
-
-# %%
-data['recon'].amax((-1, -2))
-
-# %%
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
-# %%
+
 model = Unet(2, 2)
 model.to(device)
 
-# %%
+
 loss_fn = torch.nn.MSELoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
-# %%
+
 model_weight_path = '/home/kadotab/python/ml/ml_recon/Model_Weights/'
 def train(model, loss_function, optimizer, dataloader, epoch=7):
     cur_loss = 0
@@ -84,6 +71,7 @@ def train(model, loss_function, optimizer, dataloader, epoch=7):
                     predicted_sampled = model(undersampled_slice)
 
                     predicted_sampled = torch.sqrt(predicted_sampled.sum(1).pow(2) + 1e-6)
+                    assert not predicted_sampled.isnan().any()
                     predicted_sampled = predicted_sampled[:, 160:-160, :]
                     loss = loss_function(predicted_sampled, recon_slice)
                     
@@ -105,9 +93,9 @@ def train(model, loss_function, optimizer, dataloader, epoch=7):
     except KeyboardInterrupt:
         pass
 
-    save_model(model_weight_path, model, optimizer, current_index)
+    save_model(model_weight_path, model, optimizer, -1)
 
-# %%
+
 train(model, loss_fn, optimizer, dataloader)
 
 
