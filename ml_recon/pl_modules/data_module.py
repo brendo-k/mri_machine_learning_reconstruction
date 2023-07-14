e0e4e7971b112f6e23f2a58ad960f19413e43614
Brenden <brenden.kadota@gmail.com>
2023-07-14 13:03:29 -0400

    Added pytorch lightning support (work in progress)

---
+++ ml_recon/pl_modules/data_module.py
@@ -0,0 +1,67 @@
+import os
+from typing import Callable, Union
+
+import torchvision
+import pytorch_lightning as pl
+from torch.utils.data import DataLoader
+
+from ml_recon.transforms import toTensor, normalize
+from ml_recon.dataset.slice_loader import SliceLoader
+from ml_recon.dataset.self_supervised_slice_loader import SelfSupervisedSampling
+from ml_recon.utils.read_headers import make_header
+
+
+class fastMRIDataLoader(pl.LightningDataModule):
+    def __init__(
+            self, 
+            data_dir: Union[str, os.PathLike], 
+            supervised: bool = True,
+            batch_size: int = 1, 
+            transforms: Callable = None,
+            num_workers: int = 0
+            ):
+        super().__init__()
+        self.data_dir = data_dir
+        self.batch_size = batch_size
+        self.transforms = transforms
+        self.num_workers = num_workers
+
+        self.supervised = supervised
+
+        # transforms, convert to tensor and normalize
+        self.transform = torchvision.transforms.Compose((toTensor(), normalize()))
+
+    def prepare_data(self):
+        train_dir = os.path.join(self.data_dir, 'multicoil_train')
+        test_dir = os.path.join(self.data_dir, 'multicoil_test')
+        val_dir = os.path.join(self.data_dir, 'multicoil_val')
+        if not self.header_exists(test_dir):
+            make_header(test_dir)
+        if not self.header_exists(train_dir):
+            make_header(train_dir)
+        if not self.header_exists(val_dir):
+            make_header(val_dir)
+        
+        if self.supervised:
+            # if supervised use supervised loader
+            self.fastmri_train = SliceLoader(train_dir + 'header.json')
+            self.fastmri_val = SliceLoader(val_dir + 'header.json')
+            self.fastmri_test = SliceLoader(test_dir + 'header.json')
+        else:
+            # else use self supervised loader
+            self.fastmri_train = SelfSupervisedSampling(train_dir + 'header.json')
+            self.fastmri_val = SelfSupervisedSampling(val_dir + 'header.json')
+            self.fastmri_test = SelfSupervisedSampling(test_dir + 'header.json')
+
+    def train_dataloader(self):
+        return DataLoader(self.fastmri_train, batch_size=self.batch_size, num_workers=self.num_workers)
+
+    def val_dataloader(self):
+        return DataLoader(self.fastmri_val, batch_size=self.batch_size, num_workers=self.num_workers)
+
+    def test_dataloader(self):
+        return DataLoader(self.fastmri_test, batch_size=self.batch_size, num_workers=self.num_workers)
+    
+    def header_exists(self, path):
+        assert os.path.isdir(path)
+        return os.path.isfile(path + 'header.json')
---
+++ ml_recon/pl_modules/mri_module.py
@@ -0,0 +1,85 @@
+import pytorch_lightning as pl
+
+class mri_module(pl.LightningModule):
+    def __init__(self):
+        super().__init__()
+
+    def on_validation_epoch_end(self, val_logs):
+        # check inputs
+        for k in (
+            "batch_idx",
+            "fname",
+            "slice_num",
+            "max_value",
+            "output",
+            "target",
+            "val_loss",
+        ):
+            if k not in val_logs.keys():
+                raise RuntimeError(
+                    f"Expected key {k} in dict returned by validation_step."
+                )
+        if val_logs["output"].ndim == 2:
+            val_logs["output"] = val_logs["output"].unsqueeze(0)
+        elif val_logs["output"].ndim != 3:
+            raise RuntimeError("Unexpected output size from validation_step.")
+        if val_logs["target"].ndim == 2:
+            val_logs["target"] = val_logs["target"].unsqueeze(0)
+        elif val_logs["target"].ndim != 3:
+            raise RuntimeError("Unexpected output size from validation_step.")
+
+        # pick a set of images to log if we don't have one already
+        if self.val_log_indices is None:
+            self.val_log_indices = list(
+                np.random.permutation(len(self.trainer.val_dataloaders[0]))[
+                    : self.num_log_images
+                ]
+            )
+
+        # log images to tensorboard
+        if isinstance(val_logs["batch_idx"], int):
+            batch_indices = [val_logs["batch_idx"]]
+        else:
+            batch_indices = val_logs["batch_idx"]
+        for i, batch_idx in enumerate(batch_indices):
+            if batch_idx in self.val_log_indices:
+                key = f"val_images_idx_{batch_idx}"
+                target = val_logs["target"][i].unsqueeze(0)
+                output = val_logs["output"][i].unsqueeze(0)
+                error = torch.abs(target - output)
+                output = output / output.max()
+                target = target / target.max()
+                error = error / error.max()
+                self.log_image(f"{key}/target", target)
+                self.log_image(f"{key}/reconstruction", output)
+                self.log_image(f"{key}/error", error)
+
+        # compute evaluation metrics
+        mse_vals = defaultdict(dict)
+        target_norms = defaultdict(dict)
+        ssim_vals = defaultdict(dict)
+        max_vals = dict()
+        for i, fname in enumerate(val_logs["fname"]):
+            slice_num = int(val_logs["slice_num"][i].cpu())
+            maxval = val_logs["max_value"][i].cpu().numpy()
+            output = val_logs["output"][i].cpu().numpy()
+            target = val_logs["target"][i].cpu().numpy()
+
+            mse_vals[fname][slice_num] = torch.tensor(
+                evaluate.mse(target, output)
+            ).view(1)
+            target_norms[fname][slice_num] = torch.tensor(
+                evaluate.mse(target, np.zeros_like(target))
+            ).view(1)
+            ssim_vals[fname][slice_num] = torch.tensor(
+                evaluate.ssim(target[None, ...], output[None, ...], maxval=maxval)
+            ).view(1)
+            max_vals[fname] = maxval
+
+        return {
+            "val_loss": val_logs["val_loss"],
+            "mse_vals": dict(mse_vals),
+            "target_norms": dict(target_norms),
+            "ssim_vals": dict(ssim_vals),
+            "max_vals": max_vals,
+        }
