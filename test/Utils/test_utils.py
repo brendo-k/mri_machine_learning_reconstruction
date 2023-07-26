edba8defe5d5af8ec1dc426ff640fc0b055ae99a
Brenden Kadota <brenden.kadota@gmail.com>
2023-07-26 12:55:17 -0700

    Refractoring

--- test/Utils/test_utils.py
+++ test/Utils/test_utils.py
@@ -1,11 +1,11 @@
-from ml_recon.utils.root_sum_of_squares import combine_coils
+from ml_recon.utils.root_sum_of_squares import root_sum_of_squares
 import torch
 import math
 
 def test_combine_coils_unity():
     x = torch.zeros((1, 1, 1))
     x[0, 0, 0] = 5
-    x_combined = combine_coils(x)
+    x_combined = root_sum_of_squares(x)
     assert x_combined.ndim == 2
     assert x_combined[0, 0] == 5
 
@@ -14,6 +14,6 @@ def test_combine_coils():
     x = torch.zeros((1, 2, 1))
     x[0, 0, 0] = 5
     x[0, 1, 0] = 2
-    x_combined = combine_coils(x, coil_dim=1)
+    x_combined = root_sum_of_squares(x, coil_dim=1)
     assert x_combined.ndim == 2
     assert x_combined[0, 0] == math.sqrt(5**2 + 2**2)
