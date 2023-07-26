8fdf850c95662cfddc78190517fae88500626472
Brenden Kadota <brenden.kadota@gmail.com>
2023-07-26 08:01:58 -0700

    updated unet tests

--- ml_recon/utils/__init__.py
+++ ml_recon/utils/__init__.py
@@ -1,5 +1,5 @@
 from .image_slices import image_slices
 from .fft_2d_img import fft_2d_img, ifft_2d_img
-from .root_sum_of_squares import combine_coils
+from .root_sum_of_squares import root_sum_of_squares
 from .save_model import save_model
 from .collate_function import collate_fn
