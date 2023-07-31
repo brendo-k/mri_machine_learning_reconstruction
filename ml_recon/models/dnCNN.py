080795af5f0b0629817c3e75b6c1997a9c1acc2e
Brenden Kadota <brenden.kadota@gmail.com>
2023-07-31 10:56:19 -0400

    added leaky relu and instance norm

--- ml_recon/models/dnCNN.py
+++ ml_recon/models/dnCNN.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 
 class DnCNN(nn.Module):
+
     def __init__(self, 
                  in_chan, 
                  out_chan,
@@ -16,14 +17,16 @@ class DnCNN(nn.Module):
             nn.Conv2d(in_channels=in_chan, out_channels=feature_size, kernel_size=kernel_size, padding=1, bias=False),
             nn.ReLU(inplace=True)
         )
+
         self.dncnn.append(encoder)
 
         for _ in range(num_of_layers-2):
             self.dncnn.append(nn.Conv2d(in_channels=feature_size, out_channels=feature_size, kernel_size=kernel_size, padding=1, bias=False))
-            self.dncnn.append(nn.BatchNorm2d(feature_size))
-            self.dncnn.append(nn.ReLU(inplace=True))
+            self.dncnn.append(nn.InstanceNorm2d(feature_size))
+            self.dncnn.append(nn.LeakyReLU(negative_slope=0.2, inplace=True))
 
         decoder = nn.Conv2d(in_channels=feature_size, out_channels=out_chan, kernel_size=kernel_size, padding=1, bias=False)
+
         self.dncnn.append(decoder)
 
     def forward(self, x):
