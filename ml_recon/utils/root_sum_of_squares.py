59531193c999db1fac3b9d4885957b6b68411a5b
Brenden Kadota <brenden.kadota@gmail.com>
2023-08-18 10:42:07 -0400

    Added documentation and asserts to coil combine

--- ml_recon/utils/root_sum_of_squares.py
+++ ml_recon/utils/root_sum_of_squares.py
@@ -1,4 +1,13 @@
-
 def root_sum_of_squares(data, coil_dim=0):
+    """ Takes asquare root sum of squares of the abosolute value of complex data along the coil dimension
+
+    Args:
+        data (torch.Tensor): Data needed to be coil combined
+        coil_dim (int, optional): dimension index. Defaults to 0.
+
+    Returns:
+        torch.Tensor: Coil combined data
+    """
+    assert data.ndims > coil_dim
     return data.abs().pow(2).sum(coil_dim).sqrt()
     
