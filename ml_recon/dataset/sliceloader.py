04f5ab5ad70168ea3bdaffe37b6bbe3311375c9b
Brenden Kadota <brenden.kadota@gmail.com>
2023-08-19 15:09:51 -0400

    simplified dataloading into decorator and loader

---
+++ ml_recon/dataset/sliceloader.py
@@ -0,0 +1,105 @@
+import numpy as np
+import os
+import json
+from typing import Union, Callable
+from torch.utils.data import Dataset
+from scipy.interpolate import interpn
+
+from ml_recon.dataset.filereader.read_h5 import H5FileReader
+from ml_recon.dataset.filereader.filereader import FileReader
+from ml_recon.utils.read_headers import make_header
+
+class SliceDataset(Dataset):
+    """This is a supervised slice dataloader. 
+
+    Args:
+        meta_data (str, os.PathLike): path to metadata file holding slice information,
+        acs_width (int): width of auto calibration lines to keep
+        R (int): acceleration factor to use
+    """
+    def __init__(
+            self,
+            data_dir: Union[str, os.PathLike],
+            file_reader: FileReader = H5FileReader,
+            raw_sample_filter: Callable = lambda _: True,
+            transforms: Callable = None,
+            build_new_header: bool = False
+            ):
+
+        # call super constructor
+        super().__init__()
+
+        self.data_list = []
+        self.file_reader = file_reader
+
+        header_file = os.path.join(data_dir, 'header.json')
+
+        if not os.path.isfile(header_file) or build_new_header:
+            print(f'Making header in {data_dir}')
+            self.data_list = make_header(data_dir, self.file_reader, output=header_file, sample_filter=raw_sample_filter)
+        else:    
+            with open(header_file, 'r') as f:
+                print('Header file found!')
+                self.index_info = json.load(f)
+                print(f'Found {len(self.index_info)} slices')
+                for index in self.index_info:
+                    if raw_sample_filter(self.index_info[index]):
+                        self.data_list.append(self.index_info[index])
+
+        # add transforms
+        self.transforms = transforms
+
+    def __len__(self):
+        return len(self.data_list)
+
+    def __getitem__(self, index):
+        data = self.get_data_from_file(index)
+        data = self.resample_or_pad(data)
+        assert data.shape == (16, 128, 128)
+
+        if self.transforms:
+            data = self.transforms(data)
+        return data
+    
+    def get_data_from_file(self, index):
+        slice_index = self.data_list[index]['slice_index']
+        file_name = self.data_list[index]['file_name']
+        with self.file_reader(file_name) as fr:
+            k_space = np.ascontiguousarray(np.flip(fr['kspace'][slice_index], axis=1))
+            assert not np.isnan(k_space).any()
+            recon_slice = np.ascontiguousarray(np.flip(fr['recon'][slice_index], axis=1))
+        return k_space 
+
+    def resample_or_pad(self, k_space, reduce_fov=True):
+        """Takes k-space data and resamples data to desired height and width. If 
+        the image is larger, we crop. If the image is smaller, we pad with zeros
+
+        Args:
+            k_space (np.ndarray): k_space to be cropped or padded 
+            reduce_fov (bool, optional): If we should reduce fov along readout dimension. Defaults to True.
+
+        Returns:
+            np.ndarray: cropped k_space
+        """
+        resample_height = 128
+        resample_width = 128
+        if reduce_fov:
+            k_space = k_space[:, ::2, :]
+        _, height, width = k_space.shape
+        height_cent, width_cent = int(height/2), int(width/2)
+        if height > resample_height:
+            k_space = k_space[:, height_cent - resample_height//2:height_cent + resample_height//2, :]
+        else:
+            pad_top = (resample_height - height)//2
+            pad_bottom = (resample_height - height)- pad_top
+            k_space = np.pad(k_space, [pad_top, pad_bottom, 0, 0])
+
+        if width > resample_width:
+            k_space = k_space[:, :, width_cent - resample_width//2: width_cent + resample_width//2]
+        else:
+            pad_left = (resample_width - width)//2
+            pad_right = (resample_width - width) - pad_top
+            k_space = np.pad(k_space, [0, 0, pad_left, pad_right])
+        return k_space
+
+
---
+++ test/Datasets/test_undersampling_decorator.py
@@ -0,0 +1,85 @@
+import numpy as np
+import pytest
+
+import torch
+from torch.utils.data import DataLoader
+from torchvision.transforms import Compose
+
+from ml_recon.utils.collate_function import collate_fn
+from ml_recon.transforms import to_tensor, pad, pad_recon
+from ml_recon.utils.read_headers import make_header
+from ml_recon.dataset.sliceloader import SliceDataset
+from ml_recon.dataset.undersampled_decorator import UndersamplingDecorator
+from ml_recon.dataset.filereader.read_h5 import H5FileReader
+
+
+@pytest.fixture
+def build_dataset(get_data_dir, scope='session') -> SliceDataset:
+    torch.manual_seed(0)
+    dataset = UndersamplingDecorator(SliceDataset(get_data_dir, H5FileReader), R=4)
+    return dataset
+
+@pytest.fixture
+def get_data_dir() -> str:
+    return '/home/kadotab/projects/def-mchiew/kadotab/Datasets/t1_fastMRI/multicoil_train/16_chans/multicoil_train/'
+
+def test_slice_load(build_dataset):
+    data = next(iter(build_dataset))
+    assert len(data) == 4
+
+def test_undersampled_slice(build_dataset):
+    data = next(iter(build_dataset))
+
+    phase_encode_size = data[0].shape[-1]
+    center = np.floor(phase_encode_size/2).astype(int)
+    acs = data[0][..., center - 5:center + 5]
+    assert np.nonzero(acs)
+
+# Test if we are able to batch slices. This requires some overhead by padding reconstruction and k-space so it is 
+# same number of dimensions. Pads the coil dimensions as zeros
+def test_undersampled_slice_batching(get_data_dir):
+    dataset = UndersamplingDecorator(SliceDataset(get_data_dir, H5FileReader), R=4, transforms=to_tensor())
+    dataloader = DataLoader(dataset, collate_fn=collate_fn, batch_size=5)
+    data = next(iter(dataloader))
+    assert data[0].shape[0] == 5
+    assert data[0].ndim == 4
+
+def test_probability_mask(build_dataset):
+    dataset = build_dataset
+    pdf = dataset.gen_pdf_columns(300, 600, 1/4, 8, 10)
+    
+    np.testing.assert_allclose(np.mean(pdf), 1/4, atol=1e-3)
+    assert pdf.shape == (600, 300)
+    np.testing.assert_equal(pdf[:, 150 - 5: 150 + 5], np.ones((600, 10)))
+
+def test_determanistic(get_data_dir):
+    dataset = SliceDataset(get_data_dir, R=4, deterministic=True)
+
+    data1 = dataset[0]
+    data2 = dataset[0]
+
+    np.testing.assert_equal(data1[0] != 0, data2[0] != 0)
+
+
+def test_non_determanistic(get_data_dir):
+    dataset = SliceDataset(get_data_dir, R=4, deterministic=False)
+
+    data1 = dataset[0]
+    data2 = dataset[0]
+
+    assert ((data1 != 0) == (data2 != 0)).any()
+
+def test_non_determanistic_between_slices(get_data_dir):
+    dataset = SliceDataset(get_data_dir, R=4, deterministic=False)
+
+    data1 = dataset[0]
+    data2 = dataset[1]
+
+    assert ((data1 != 0) == (data2 != 0)).any()
+
+    dataset = SliceDataset(get_data_dir, R=4, deterministic=True)
+
+    data1 = dataset[0]
+    data2 = dataset[1]
+    
+    assert ((data1 != 0) == (data2 != 0)).any()
