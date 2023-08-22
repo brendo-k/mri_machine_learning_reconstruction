ff47ead0c4f8e6b0207dbf44453b3201612f1a11
Brenden Kadota <brenden.kadota@gmail.com>
2023-08-22 09:54:16 -0400

    Changed save mdoel path

--- ml_recon/utils/save_model.py
+++ ml_recon/utils/save_model.py
@@ -4,10 +4,8 @@ import torch
 
 def save_model(path, model, optimizer, e, rank=0):
     if rank is None or rank == 0:
-        model_name = model.__class__.__name__
-        date = datetime.now().strftime("%Y%m%d-%H%M%S")
         torch.save({
             'model': model.state_dict(), 
             'optimizer': optimizer.state_dict(), 
             'epoch': e
-            }, path + date + model_name + '.pt')
+            }, path + e + '.pt')
