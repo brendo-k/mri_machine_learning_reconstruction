072710d531c233175856011924b2c15c66a4bfc5
Brenden Kadota <brenden.kadota@gmail.com>
2023-10-29 23:04:21 -0400

    interpolte phase maps in polar coordinates using nearest neighbours

--- plot_undersampled.py
+++ plot_undersampled.py
@@ -1,24 +1,28 @@
 from ml_recon.dataset.kspace_brats import KSpaceBrats
 from ml_recon.dataset.simulated_brats_dataset import SimulatedBrats
 from ml_recon.dataset.self_supervised_decorator import UndersampleDecorator
-from ml_recon.utils import image_slices, ifft_2d_img
+from ml_recon.utils import image_slices, ifft_2d_img, root_sum_of_squares
 import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
-    data_dir = '/home/kadotab/projects/def-mchiew/kadotab/Datasets/Brats_2021/brats/training_data/subset/train/'
+    data_dir = '/home/kadotab/projects/def-mchiew/kadotab/Datasets/Brats_2021/brats/training_data/simulated_subset/train/'
 
-    sim_brats = SimulatedBrats(data_dir)
+    sim_brats = KSpaceBrats(data_dir)
 
     dataset = UndersampleDecorator(sim_brats)
 
     doub_under, under, k_space, k = dataset[0]
 
     plt.figure(1)
-    plt.imshow(ifft_2d_img(k_space[0, 2]).abs(), cmap='gray')
+    image_slices(root_sum_of_squares(ifft_2d_img(k_space), coil_dim=1).abs(), cmap='gray')
     plt.figure(2)
-    plt.imshow(ifft_2d_img(under[0, 2]).abs(), cmap='gray')
+    image_slices(ifft_2d_img(under[0]).abs(), cmap='gray')
     plt.figure(3)
-    plt.imshow(ifft_2d_img(doub_under[0, 2]).abs(), cmap='gray')
+    image_slices(ifft_2d_img(doub_under[0]).abs(), cmap='gray')
+    plt.figure(4)
+    
+
+
     plt.show()
     #image_slices(ifft_2d_img(doub_under[0]).abs())
     #plt.savefig('doub_undersampled.png')
