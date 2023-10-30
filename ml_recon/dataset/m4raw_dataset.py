9038eacd19bd566d55529194601d9cbdbb45efa9
Brenden Kadota <brenden.kadota@gmail.com>
2023-10-30 01:36:46 -0400

    updated brats for contarsts

--- ml_recon/dataset/m4raw_dataset.py
+++ ml_recon/dataset/m4raw_dataset.py
@@ -1,17 +1,12 @@
 import numpy as np
 import os
-import json
-from typing import Union, Callable
-from torch.utils.data import Dataset
+from typing import Union, Callable, Collection
 import torchvision.transforms.functional as F
 import torch
 import h5py
-import random
 from argparse import ArgumentParser
 
 from ml_recon.dataset.k_space_dataset import KSpaceDataset
-from ml_recon.utils.read_headers import make_header
-from ml_recon.dataset.undersample import gen_pdf_columns, calc_k, apply_undersampling
 
 class M4Raw(KSpaceDataset):
     """This is a dataloader for m4Raw. All it does is load a slice from the M4Raw 
@@ -25,6 +20,7 @@ class M4Raw(KSpaceDataset):
             nx:int = 256,
             ny:int = 256,
             transforms: Union[Callable, None] = None, 
+            contrasts: Collection[str] = ['t1', 't2', 'flair']
             ):
 
         # call super constructor
@@ -43,19 +39,27 @@ class M4Raw(KSpaceDataset):
         for patient in patient_id:
             patient_files = [os.path.join(data_dir, file) for file in files if patient in file]
             patient_files.sort()
-            if first: 
-                self.contrast_order = [file.split('-')[1] for file in patient_files]
-                first = False
 
+            if first: 
+                self.contrast_order = np.array([file.split('-')[-1].split('.')[0] for file in patient_files])
+                first = False
             
-            self.file_names.append(patient_files)
+            self.file_names.append(np.array(patient_files))
 
             with h5py.File(patient_files[0], 'r') as fr:
                 slices.append(fr['kspace'].shape[0])
 
+
+        contrasts = np.array(contrasts)
+        print(self.contrast_order)
+        self.contrast_index = np.isin(self.contrast_order, contrasts)
+        print(self.contrast_index)
+        self.contrasts = self.contrast_order[self.contrast_index]
+
         self.slice_cumulative_sum = np.cumsum(slices) 
         self.length = self.slice_cumulative_sum[-1]
         print(f'Found {self.length} slices!')
+        print(f'Found {self.contrasts} slices!')
 
 
     def __len__(self):
@@ -73,6 +77,8 @@ class M4Raw(KSpaceDataset):
         volume_index = np.sum(self.slice_cumulative_sum <= index)
         slice_index = index if volume_index == 0 else index - self.slice_cumulative_sum[volume_index - 1]
         cur_files = self.file_names[volume_index]
+        print(cur_files)
+        cur_files = cur_files[self.contrast_index]
         
         k_space = []
         
@@ -117,12 +123,15 @@ import matplotlib.pyplot as plt
 from ml_recon.utils import image_slices, root_sum_of_squares, ifft_2d_img
 if __name__ == '__main__':
     dir = '/home/kadotab/projects/def-mchiew/kadotab/Datasets/M4raw/multicoil_train_averaged/'
-    dataset = M4Raw(dir)
+    dataset = M4Raw(dir, contrasts=['t1', 't2'])
     l = dataset[0]
 
     image_slices(l[0].abs(), cmap='gray')
     image_slices(ifft_2d_img(l[0]).abs(), cmap='gray')
-    image_slices(root_sum_of_squares(ifft_2d_img(l), coil_dim=1), cmap='gray')
+    plt.figure(3)
+    plt.imshow(root_sum_of_squares(ifft_2d_img(l[0]), coil_dim=0), cmap='gray')
+    plt.figure(4)
+    plt.imshow(root_sum_of_squares(ifft_2d_img(l[1]), coil_dim=0), cmap='gray')
     plt.show()
 
 
