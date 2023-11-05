4e93b04aa0a96826a240f6823dedd4086c115d30
Brenden Kadota <brenden.kadota@gmail.com>
2023-11-05 09:15:24 -0800

    added flag for normalization method

--- ml_recon/transforms/transforms.py
+++ ml_recon/transforms/transforms.py
@@ -6,8 +6,8 @@ import einops
 
 # Normalize to [0, 1] range per contrast
 class normalize(object):
-    def __init__(self, norm_mean=False):
-        self.norm_mean=norm_mean
+    def __init__(self, norm_method='mean'):
+        self.norm_method=norm_method
 
     def __call__(self, sample):
         doub_under, under, sampled, k = sample
@@ -15,10 +15,12 @@ class normalize(object):
         image = root_sum_of_squares(ifft_2d_img(under, axes=[-1, -2]), coil_dim=1)
         assert isinstance(image, torch.Tensor)
 
-        if self.norm_mean:
+        if self.norm_method == 'mean':
             undersample_max = image.mean((1, 2), keepdim=True).unsqueeze(1)
-        else:
+        elif self.norm_method == 'std':
             undersample_max = image.std((1, 2), keepdim=True).unsqueeze(1)
+        elif self.norm_method == 'max':
+            undersample_max = image.amax((1, 2), keepdim=True).unsqueeze(1)
 
         return (doub_under/undersample_max, under/undersample_max, sampled/undersample_max, k)
 
