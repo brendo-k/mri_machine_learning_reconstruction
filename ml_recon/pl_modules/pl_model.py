fixed variable R scaling

ml_recon/pl_modules/pl_model.py
@@ -62,17 +62,19 @@ class plReconModel(pl.LightningModule):
         self.log('metrics/mean_nmse', total_nmse/len(self.contrast_order), on_epoch=True)
 
 
-    def plot_images(self, under_k, estimate_k, k_space, mask, mode='train'):
+    def plot_images(self, under_k, estimate_k, target, k_space, mask, mode='train'):
         with torch.no_grad():
             
             estimated_image = root_sum_of_squares(ifft_2d_img(estimate_k), coil_dim=2)
             image = root_sum_of_squares(ifft_2d_img(k_space), coil_dim=2)
+            target = root_sum_of_squares(ifft_2d_img(target), coil_dim=2)
 
             estimated_image = estimated_image[0]/image[0].amax((-1, -2), keepdim=True)
             image = image[0]/image[0].amax((-1, -2), keepdim=True)
+            target = target[0]/target[0].amax((-1, -2), keepdim=True)
             diff = (estimated_image - image).abs()*10
-            k_space_scaled = k_space.abs()/(k_space.abs().max() / 20) 
-            under_k = under_k.abs()/(under_k.abs().max() / 20)
+            k_space_scaled = k_space.abs()/(k_space.abs().max() / 50) 
+            under_k = under_k.abs()/(under_k.abs().max() / 50)
 
             estimated_image = estimated_image.clamp(0, 1)
             image = image.clamp(0, 1)
@@ -83,7 +85,8 @@ class plReconModel(pl.LightningModule):
 
             contrasts = estimated_image.shape[0]
             wandb_logger.log_image(mode + '/recon', np.split(estimated_image.unsqueeze(1).cpu().numpy(), contrasts, 0))
-            wandb_logger.log_image(mode + '/target', np.split(image.unsqueeze(1).cpu().numpy(), contrasts, 0))
+            wandb_logger.log_image(mode + '/fully_sampled', np.split(image.unsqueeze(1).cpu().numpy(), contrasts, 0))
+            wandb_logger.log_image(mode + '/target', np.split(target.unsqueeze(1).cpu().numpy(), contrasts, 0))
             wandb_logger.log_image(mode + '/k', [k_space_scaled[0, 0, [0]].clamp(0, 1).cpu().numpy()])
             wandb_logger.log_image(mode + '/under_k', [under_k[0, 0, [0]].clamp(0, 1).cpu().numpy()])
             wandb_logger.log_image(mode + '/mask', np.split(mask[0, :, [0], :, :].cpu().numpy(), contrasts, 0))