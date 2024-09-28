757416156dcdb423631b99705c0dad3479afe044
Brenden <brenden.kadota@gmail.com>
2024-09-27 19:07:18 -0700

    Updated image_slices for one contrast

--- ml_recon/utils/image_slices.py
+++ ml_recon/utils/image_slices.py
@@ -9,6 +9,19 @@ def image_slices(data: np.ndarray, coil_num=0, vmin=None, vmax=None, cmap=None):
         data = data[:, np.newaxis, :, :]
     
     slices = data.shape[0]
+    if slices == 1:
+        fig, axes = plt.subplots(1, 1, figsize=(10, 10))
+        current_slice = np.squeeze(data[0, coil_num, :, :])
+        axes.imshow(current_slice, vmin=vmin, vmax=vmax, cmap=cmap, aspect='equal')
+        axes.tick_params(
+                                which='both',      # both major and minor ticks are affected
+                                bottom=False,      # ticks along the bottom edge are off
+                                top=False,         # ticks along the top edge are off
+                                left=False,
+                                labelleft=False,
+                                labelbottom=False) 
+        return fig, axes
+
     width = int(np.ceil(np.sqrt(slices)))
     height = int(np.ceil(slices/width))
 
@@ -18,13 +31,13 @@ def image_slices(data: np.ndarray, coil_num=0, vmin=None, vmax=None, cmap=None):
         (x, y) = np.unravel_index(i, (width, height))
         current_slice = np.squeeze(data[i, coil_num, :, :])
         axes[x, y].imshow(current_slice, vmin=vmin, vmax=vmax, cmap=cmap, aspect='equal')
-        axes[x,y].tick_params(
-        which='both',      # both major and minor ticks are affected
-        bottom=False,      # ticks along the bottom edge are off
-        top=False,         # ticks along the top edge are off
-        left=False,
-        labelleft=False,
-        labelbottom=False) 
+        axes[x, y].tick_params(
+                                which='both',      # both major and minor ticks are affected
+                                bottom=False,      # ticks along the bottom edge are off
+                                top=False,         # ticks along the top edge are off
+                                left=False,
+                                labelleft=False,
+                                labelbottom=False) 
 
     return fig, axes
 
