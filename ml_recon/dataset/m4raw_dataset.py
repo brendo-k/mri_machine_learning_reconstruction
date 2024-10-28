Fixed scaling issues

ml_recon/dataset/m4raw_dataset.py
@@ -82,9 +82,9 @@ class M4Raw(Dataset):
             with h5py.File(file) as fr:
                 dataset = fr['kspace']
                 assert isinstance(dataset, h5py.Dataset)
-                k_space.append(torch.as_tensor(dataset[slice_index]))
+                k_space.append((dataset[slice_index]))
 
-        k_space = torch.stack(k_space, dim=0)
+        k_space = np.stack(k_space, axis=0)
         return k_space 
 
     def resample_or_pad(self, k_space):
@@ -101,4 +101,4 @@ class M4Raw(Dataset):
         resample_height = self.ny
         resample_width = self.nx
 
-        return F.center_crop(k_space, [resample_height, resample_width])
+        return F.center_crop(torch.from_numpy(k_space), [resample_height, resample_width]).numpy()

ml_recon/dataset/undersample_decorator.py
@@ -84,7 +84,7 @@ class UndersampleDecorator(Dataset):
         output = TrainingSample(
                 input = torch.from_numpy(under), 
                 target = torch.from_numpy(k_space), 
-                fs_k_space = torch.from_numpy(k_space),
+                fs_k_space = torch.from_numpy(k_space).clone(),
                 mask = torch.from_numpy(mask_omega),
                 loss_mask = torch.ones_like(torch.from_numpy(mask_omega))
                 )
@@ -106,7 +106,7 @@ class UndersampleDecorator(Dataset):
             output = TrainingSample(
                 input = torch.from_numpy(doub_under), 
                 target = torch.from_numpy(target), 
-                fs_k_space = torch.from_numpy(k_space),
+                fs_k_space = torch.from_numpy(k_space).clone(),
                 mask = torch.from_numpy(mask_lambda & mask_omega),
                 loss_mask = torch.from_numpy(~mask_lambda & mask_omega)
                 )

ml_recon/pl_modules/pl_varnet.py
@@ -134,7 +134,7 @@ class pl_VarNet(plReconModel):
             sense_maps = self.model.sens_model(under_k, under_k != 0)
             sense_maps = sense_maps[0, 0, :, :, :].unsqueeze(1).abs()
             masked_k = self.model.sens_model.mask(under_k, sampling_mask.expand_as(under_k))
-            masked_k = masked_k[0, 0, [0], :, :].abs()/(masked_k[0, 0, [0], :, :].abs().max()/20)
+            masked_k = masked_k[0, 0, [0], :, :].abs()**0.2
 
             wandb_logger = self.logger
             wandb_logger.log_image(mode + '/sense_maps', np.split(sense_maps.cpu().numpy()/sense_maps.max().item(), sense_maps.shape[0], 0))