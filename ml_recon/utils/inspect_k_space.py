9ebffe1951970e301a5fed7972790df7b9a0660f
Brenden Kadota <brenden.kadota@gmail.com>
2024-11-21 21:50:02 -0800

    added tool for looking at data

---
+++ ml_recon/utils/inspect_k_space.py
@@ -0,0 +1,35 @@
+import h5py
+import numpy as np
+import matplotlib.pyplot as plt
+from ml_recon.utils import image_slices, root_sum_of_squares, ifft_2d_img
+import argparse
+
+
+
+def main():
+    parser = argparse.ArgumentParser(description='My command-line script')
+    parser.add_argument('-f', '--filename', type=str, help='filename', required=True)
+    parser.add_argument('--images', action='store_true', help='flag for image_space')
+    parser.add_argument('-c', '--contrast_index', type=int, help='index for contrast to plot', default=0)
+
+    args = parser.parse_args()
+
+    with h5py.File(args.filename) as fr:
+        kspace = fr['k_space'][:]
+
+    print(kspace.shape)
+    if args.images: 
+        plotting_aray = root_sum_of_squares(ifft_2d_img(kspace), coil_dim=2)[:, args.contrast_index]
+    else: 
+        plotting_aray = np.abs(kspace[:, args.contrast_index, 0, :, :])**0.02
+
+    image_slices(plotting_aray, cmap='gray')
+    plt.show()
+
+        
+
+
+if __name__ == "__main__":
+    main()
+
+
