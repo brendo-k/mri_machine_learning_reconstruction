Fixed non-training due to normalization

ml_recon/models/SensetivityModel_mc.py
@@ -60,18 +60,18 @@ class SensetivityModel_mc(nn.Module):
             images = einops.rearrange(images, 'b contrast c h w -> (b contrast c) 1 h w')
         assert isinstance(images, torch.Tensor)
 
-        # norm 
-        images, mean, std = self.norm(images)
         # convert to real numbers [b * contrast * coils, cmplx, h, w]
         images = complex_to_real(images)
+        # norm 
+        images, mean, std = self.norm(images)
         assert not torch.isnan(images).any()
         # pass through model
         images = self.model(images)
         assert not torch.isnan(images).any()
-        # convert back to complex
-        images = real_to_complex(images)
         # unnorm
         images = self.unnorm(images, mean, std)
+        # convert back to complex
+        images = real_to_complex(images)
         # rearange back to original format
         if self.sensetivty_estimation == 'joint':
             images = einops.rearrange(images, '(b c) contrast h w -> b c contrast h w', c=number_of_coils, contrast=num_contrasts)
@@ -123,8 +123,14 @@ class SensetivityModel_mc(nn.Module):
         return coil_k_spaces * center_mask.unsqueeze(2)
 
     def norm(self, x: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
-        mean = x.abs().mean(dim=(-1, -2), keepdim=True)
-        std = x.abs().std(dim=(-1, -2), keepdim=True)
+        # group norm
+        b, c, h, w = x.shape
+        x = x.view(b, 2, c // 2 * h * w)
+
+        mean = x.mean(dim=2).view(b, 2, 1, 1)
+        std = x.std(dim=2).view(b, 2, 1, 1) + 1e-20
+
+        x = x.view(b, c, h, w)
 
         return (x - mean) / std, mean, std
 

ml_recon/pl_modules/pl_varnet.py
@@ -60,8 +60,8 @@ class pl_VarNet(plReconModel):
 
         prediction = self.forward(batch)
 
-        target_img = root_sum_of_squares(ifft_2d_img(batch['target']), coil_dim=1)
-        estimated_img = root_sum_of_squares(ifft_2d_img(prediction), coil_dim=1)
+        target_img = root_sum_of_squares(ifft_2d_img(batch['target']), coil_dim=2)
+        estimated_img = root_sum_of_squares(ifft_2d_img(prediction), coil_dim=2)
         
         loss = torch.tensor([0], dtype=torch.float32, device=self.device)
         
@@ -166,9 +166,9 @@ class pl_VarNet(plReconModel):
         if k_loss_function == 'norml1l2':
             loss_func = L1L2Loss(norm_all_k)
         elif k_loss_function == 'l1':
-            loss_func = torch.nn.L1Loss(reduce='sum')
+            loss_func = torch.nn.L1Loss(reduction='sum')
         elif k_loss_function == 'l2':
-            loss_func = torch.nn.MSELoss(reduce='sum')
+            loss_func = torch.nn.MSELoss(reduction='sum')
         else:
             print('No k-space loss!!!')
             return None