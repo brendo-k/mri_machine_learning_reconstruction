34460c7b04ebc8295bbb2f39fed18b22d410220d
Brenden Kadota <brenden.kadota@gmail.com>
2025-07-07 07:23:33 -0400

    shuffle dataloader

--- ml_recon/pl_modules/pl_UndersampledDataModule.py
+++ ml_recon/pl_modules/pl_UndersampledDataModule.py
@@ -136,7 +136,7 @@ class UndersampledDataModule(pl.LightningDataModule):
             self.train_dataset, 
             batch_size=self.batch_size, 
             num_workers=self.num_workers,
-            shuffle=False,
+            shuffle=True,
             pin_memory=True
         )
 
